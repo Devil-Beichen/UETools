78d7402f0d236c75b708194ca8e5e3f1866c7136
陈华 <2394439184@qq.com>
2024-10-30 15:03:25 +0800

    围栏城墙相关的模块创建

---
+++ Plugins/ToolKits/Source/FenceWallRelated/FenceWallRelated.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class FenceWallRelated : ModuleRules
+{
+    public FenceWallRelated(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Plugins/ToolKits/Source/FenceWallRelated/Private/FenceWallRelated.cpp
@@ -0,0 +1,17 @@
+﻿#include "FenceWallRelated.h"
+
+#define LOCTEXT_NAMESPACE "FFenceWallRelatedModule"
+
+void FFenceWallRelatedModule::StartupModule()
+{
+    
+}
+
+void FFenceWallRelatedModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FFenceWallRelatedModule, FenceWallRelated)
