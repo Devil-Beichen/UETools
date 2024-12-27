732311c2490a8ba6e7f6a25fad86d2f8029acba2
陈华 <2394439184@qq.com>
2024-12-27 14:56:22 +0800

    战争迷雾模块添加

---
+++ Plugins/ToolKits/Source/FogOfWar/FogOfWar.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class FogOfWar : ModuleRules
+{
+    public FogOfWar(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Plugins/ToolKits/Source/FogOfWar/Private/FogOfWar.cpp
@@ -0,0 +1,17 @@
+﻿#include "FogOfWar.h"
+
+#define LOCTEXT_NAMESPACE "FFogOfWarModule"
+
+void FFogOfWarModule::StartupModule()
+{
+    
+}
+
+void FFogOfWarModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FFogOfWarModule, FogOfWar)
